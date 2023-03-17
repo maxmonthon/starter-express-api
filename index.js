a760af0a78cf641f98b25c9acf32b001af92b033
maxmonthon <70101075+maxmonthon@users.noreply.github.com>
2023-03-17 16:42:54 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import express from "express";
+import supplierRoute from "./routes/supplierRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cors from "cors";
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("test api");
+});
+app.use(cors());
+app.use(express.json());
+app.use("/suppliers", supplierRoute);
+app.use("/products", productRoute);
+
+app.listen(PORT, () => {
+  console.log("Server running on port : " + PORT);
+});
+
+// app.listen("/", (req, res) => {
+//   console.log("Run server!");
+//   res.send("Yo!");
+// });
+
+// app.listen(process.env.PORT || 5000);
